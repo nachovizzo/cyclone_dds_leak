606cc33d312230d8641b708610b76d9c7b089a49
Ignacio Vizzo <ignacio@dexory.com>
2023-11-13 10:27:37 +0100

    Add bash script

---
+++ leak.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# In 10 steps spit a string payload to the /parameter_events for each leaky node
+
+# a big string
+payload=$(printf 'X%.0s' {1..100000})
+
+for step in {1..10}; do
+    echo "Leaking step $step ..."
+    for i in {0..9}; do
+        ros2 param set /leaky_node${i} my_parameter ${payload} >/dev/null &
+    done
+    sleep 5
+done
