6462a65592068fd35edf15f58d7d1d542bcb12be
Ignacio Vizzo <ignacio@dexory.com>
2023-11-13 11:29:24 +0100

    Tweak params

--- plot.py
+++ plot.py
@@ -1,35 +1,22 @@
-import argparse
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def main(filename):
+def main():
     # Extract the column you want to plot
-    data = np.genfromtxt(filename, delimiter=",")
+    cyclonedds = np.genfromtxt("cyclonedds.csv", delimiter=",")
+    fastdds = np.genfromtxt("fastdds.csv", delimiter=",")
 
     # Plot the data
-    plt.plot(data)
+    plt.plot(cyclonedds)
+    plt.plot(fastdds)
 
     # Add labels and title
     plt.xlabel("time")
     plt.ylabel("memory usage in MiB")
-    plt.title(filename)
-
-    # Show the plot
+    plt.legend()
     plt.show()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Simple script to log memory consumption based on process name",
-    )
-    parser.add_argument(
-        "-f",
-        "--filename",
-        default="results.csv",
-        help="Specify the input filename",
-    )
-
-    args = parser.parse_args()
-    main(args.filename)
+    main()
--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # build just in case :)
+# cleanup, sorry!
+rm *.csv
+
+# build just in case :)
 colcon build
 
 # First stop, try with CycloneDDS
@@ -10,3 +14,6 @@ RMW_IMPLEMENTATION=rmw_cyclonedds_cpp tmuxinator start project mapping -p cyclon
 # Now go with FastDDS
 unset RMW_IMPLEMENTATION
 RMW_IMPLEMENTATION=rmw_fastrtps_cpp tmuxinator start project mapping -p fastdds.yaml
+
+## Plot results
+python3 ./plot.py
