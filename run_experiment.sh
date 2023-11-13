286bde99dfae66fa0edac70bc00e97883e04126e
Ignacio Vizzo <ignacio@dexory.com>
2023-11-13 11:42:42 +0100

    Update

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,16 +4,24 @@
 # cleanup, sorry!
 rm *.csv 2>/dev/null
 
+# Kill ros2cli to avoid DDS crazyness
+pkill -9 $(pgrep -f '/usr/bin/python3 -c from ros2cli.daemon.daemonize') 2>/dev/null
+
 # build just in case :)
 colcon build
 
 # First stop, try with CycloneDDS
 unset RMW_IMPLEMENTATION
-RMW_IMPLEMENTATION=rmw_cyclonedds_cpp tmuxinator start project mapping -p cyclonedds.yaml
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+tmuxinator start project mapping -p cyclonedds.yaml
+
+# Kill ros2cli to avoid DDS crazyness
+pkill -9 $(pgrep -f '/usr/bin/python3 -c from ros2cli.daemon.daemonize') 2>/dev/null
 
 # Now go with FastDDS
 unset RMW_IMPLEMENTATION
-RMW_IMPLEMENTATION=rmw_fastrtps_cpp tmuxinator start project mapping -p fastdds.yaml
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+tmuxinator start project mapping -p fastdds.yaml
 
 ## Plot results
 python3 ./plot.py
